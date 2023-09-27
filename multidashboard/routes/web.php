2b29a106a4bc4d45e5b09e279783e08b18b709af
mannaaditya <adityamanna21@gmail.com>
2023-09-28 00:39:36 +0530

    finaldashboard

---
+++ multidashboard/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Admin\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/',[AuthController::class,'loadlogin']);
+Route::get('/login',[AuthController::class,'login']);
+Route::get('/logout',[AuthController::class,'logout']);
+
+Route::group(['prefix' =>'Admin','middleware'=>['web','isAdmin']],function(){
+    Route::get('/dashboard',[AdminController::class,'dashboard']);
+
+});
+Route::group(['prefix' =>'User','middleware'=>['web','isUser']],function(){
+    Route::get('/dashboard',[UserController ::class,'dashboard']);
+
+});
